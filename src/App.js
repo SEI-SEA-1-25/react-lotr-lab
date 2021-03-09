e01017bf4569c6ef276ae67b27e6e19cdf06768c
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-03-08 21:38:14 -0800

    Finish hw

--- src/App.js
+++ src/App.js
@@ -8,6 +8,18 @@ const Fellowship = {
   minutes: 58
 }
 
+const Towers = {
+  title: "The Two Towers",
+  hours: 2,
+  minutes: 59
+}
+
+const Return = {
+  title: "The Return of the King",
+  hours: 3,
+  minutes: 21
+}
+
 function App() {
   return (
     <div className="App">
@@ -16,7 +28,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
         <Movie title={Fellowship.title} hours={Fellowship.hours} minutes={Fellowship.minutes} />
+        <Movie title={Towers.title} hours={Towers.hours} minutes={Towers.minutes} />
+        <Movie title={Return.title} hours={Return.hours} minutes={Return.minutes} />
+        
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/Movie.js
+++ src/Movie.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class Movie extends Component {
     render() {
         return(
-            <div class="lotr-style">
-                <h1>{this.props.title}</h1>
+            <div>
+                <h1>Lord of the Rings: {this.props.title}</h1>
                 <p>{this.props.hours}h {this.props.minutes}min</p>
             </div>
         )
